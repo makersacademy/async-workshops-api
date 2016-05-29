12c30a4f0f58e41966382876ef8d2be409a1fa5c
Mary Rose Cook <mary@maryrosecook.com>
2016-05-29 19:55:08 +0100

    Remove unnecessary parens

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/person/:id', function(request, response) {
 
 // start app
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
